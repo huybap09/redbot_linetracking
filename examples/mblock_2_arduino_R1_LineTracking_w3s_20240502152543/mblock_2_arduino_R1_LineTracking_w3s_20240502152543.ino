7b30b8307420563530796bf8b542705ae3f6671a
MinhKhanh614 <0308231027@caothang.edu.vn>
2024-05-03 12:48:20 +0700

    da sua code trong file w3s.ino va readme.md

--- examples/mblock_2_arduino_R1_LineTracking_w3s_20240502152543/mblock_2_arduino_R1_LineTracking_w3s_20240502152543.ino
+++ examples/mblock_2_arduino_R1_LineTracking_w3s_20240502152543/mblock_2_arduino_R1_LineTracking_w3s_20240502152543.ino
@@ -1,5 +1,3 @@
-
-
 #include <Arduino.h>
 #include "R1_VAR.h"
 
@@ -10,30 +8,27 @@ float threshold_left;
 float last_lost_dir;
 float left_dir;
 float speedForward;
-float speedBackward;
 float threshold_right;
 float right_dir;
 float threshold_mid;
 float mid_dir;
-
+float speedForwardlow;
+float stopspeed;
 
 void setup(){
     threshold_left =  0.5;
     threshold_right = 0.5;
     threshold_mid = 0.5;
-
     left_dir = 0;
     right_dir = 1;
     mid_dir = 3;
     last_lost_dir = left_dir;
     speedForward = 45;
-    int speedForwardlow = speedForward/2;
-    int stopspeed = 0;
-    speedBackward = 20;
-    while(1) {
-
-
-        eye_Left_Value = digitalRead(A3);
+    speedForwardlow = speedForward/2;
+    stopspeed = 0;
+}
+void loop(){
+  eye_Left_Value = digitalRead(A3);
         eye_Right_Value = digitalRead(A1);
         eye_Mid_Value = digitalRead(A2);
         //
@@ -79,10 +74,5 @@ void setup(){
         }
 
     }
-
 }
-}
-}
-
-void loop(){
 }
